56c58a2a89d1632f55d1e42c5fe90307b1da05fc
Ubuntu <ubuntu@ip-172-31-34-55.eu-north-1.compute.internal>
2024-08-05 09:08:53 +0500

    PORT changed in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
-  await app.listen(5000);
+  await app.listen(5001);
 }
 
 bootstrap();
